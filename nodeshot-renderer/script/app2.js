bf25090d2c4f51a81bf7a4bac76c442f7ab7df7f
Jaken <chenlong.lt@aliyun.com>
2015-10-03 20:49:30 +0800

    u

--- nodeshot-renderer/script/app2.js
+++ nodeshot-renderer/script/app2.js
@@ -1,6 +1,4 @@
-var ScreenshotApi = require('./lib/ScreenshotApi.js'),
-
-    fs = require('fs'),
+var    fs = require('fs'),
     path = require('path'),
 
     winston = require('winston'),
@@ -8,7 +6,6 @@ var ScreenshotApi = require('./lib/ScreenshotApi.js'),
 
     kue = require('kue'),
     jobs = kue.createQueue();
-var screenshot = require('node-webkit-screenshot');
 
 var webshot = require('webshot');
 var stream        = require("stream");
@@ -27,43 +24,41 @@ var options = {
     }
     , userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
     , timeout: 10000
+    , renderDelay: 1000
+    , phantomConfig: {
+        'ignore-ssl-errors': 'true',
+        'local-to-remote-url-access': 'true'
+    }
 };
-webshot('http://mp.weixin.qq.com/s?__biz=MzI1NzAxNTI3Mw==&mid=211339224&idx=5&sn=b05e7797b14c26cb7ef7aac65d0c3510&3rd=MzA3MDU4NTYzMw==&scene=6#rd', 'baidu2244.jpeg', options, function(err) {
+var options2 = {
+    screenSize: {
+        width: 320
+        , height: 3080
+    }
+    , shotSize: {
+        width: 320
+        , height: 'all'
+    }
+    , userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+    , timeout: 10000
+    , renderDelay: 2000
+};
+var options3 = {
+    screenSize: {
+        width: 1024
+        , height: 3048
+    }
+    , shotSize: {
+        width: 1024
+        , height: 'all'
+    }
+    , errorIfStatusIsNot200: true
+    , renderDelay: 2000
+};
+console.log('start');
+// http://mp.weixin.qq.com/s?__biz=MzA5MzI0OTgwNQ==&mid=229729259&idx=1&sn=b481300abc797fffaac5efafaa2cfb52&3rd=MzA3MDU4NTYzMw==&scene=6#rd
+webshot('http://mp.weixin.qq.com/s?__biz=MzA5MzI0OTgwNQ==&mid=229729259&idx=1&sn=b481300abc797fffaac5efafaa2cfb52&3rd=MzA3MDU4NTYzMw==&scene=6#rd', 'sogou.jpeg', options2, function(err) {
     winston.info('err "%s"', JSON.stringify(err));
 });
 
 winston.info('--- Starting end ---');
-var screenshotApi = new ScreenshotApi(config.screenshot);
-
-jobs.process('screenshot', function (job, done) {
-    winston.info('Process job "%s"', job.data.title);
-
-    var canceled = false,
-        globalTimeout = setTimeout(function () {
-            canceled = true;
-            done(new Error('Request took longer than ' + config.screenshot.globaltimeout + 'ms'));
-        }, config.screenshot.globaltimeout);
-
-    try {
-
-        screenshotApi.screenshot(job.data.title, job.data.options, job, function (err, stream) {
-            clearTimeout(globalTimeout);
-            if (canceled) return;
-
-            if (err) {
-
-                done('' + err);
-
-            } else {
-
-                job.log('Cache Request ' + job.data.id);
-                job.log('Done');
-
-                done();
-
-            }
-        });
-    } catch (err) {
-        done('' + err);
-    }
-});
